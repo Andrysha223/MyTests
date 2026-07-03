b7223d16ca5e6564357dea2066b99e4fa84fa058
OzhDomosh <olpakmgtd@gmail.com>
2026-07-03 18:35:19 +0300

    fix

--- tests/checkout/place-order-ukrposhta.spec.ts
+++ tests/checkout/place-order-ukrposhta.spec.ts
@@ -166,7 +166,7 @@ test('Оформление заказа с доставкою у відділе�
       name: PRODUCT_NAME,
       code: productCodeFromApi,
       price: productPriceFromApi,
-      quantity: 2,
+      quantity: 1,
     });
 
     // Стоимость доставки на странице подтверждения должна совпадать с той,
